72a05bcacbd3dea4e674b3af5b0c27514dd0242e
Dave Dove <info@dovedesign.io>
2024-10-11 11:00:28 +0100

    pcc changes

--- src/routes/work/durham-pcc/+page.svelte
+++ src/routes/work/durham-pcc/+page.svelte
@@ -4,19 +4,19 @@
 
 <svelte:head>
   <title
-    >Durham Police & Crime Commissioner | Dove Design Ltd | Real Client Success
-    Stories</title
+    >Durham Police and Crime Commissioner | Dove Design Ltd | Real Client
+    Success Stories</title
   >
   <meta
     name="description"
-    content="This page showcases the Durham Police & Crime Commissioner website that was build to help the service to offer services to help support people in the community."
+    content="This page showcases the Durham Police and Crime Commissioner website that was build to help the service to offer services to help support people in the community."
   />
-  <meta property="og:image" content="../images/open-graph.jpg" />
+  <meta property="og:image" content="./images/open-graph.jpg" />
   <meta property="og:image:width" content="1200" />
   <meta property="og:image:height" content="630" />
   <meta property="og:image:alt" content="Dove Design Ltd" />
   <meta property="og:image:type" content="image/jpeg" />
-  <meta property="og:image:secure_url" content="../images/open-graph.jpg" />
+  <meta property="og:image:secure_url" content="./images/open-graph.jpg" />
 </svelte:head>
 
 <section>
