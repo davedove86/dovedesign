4e4a56c80b28abcd26cf8e409e0c48fcabbd62b0
Dave Dove <info@dovedesign.io>
2024-10-11 11:05:36 +0100

    back to root

--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.772ac9c2.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.2226f978.js","_app/immutable/chunks/singletons.1a4a20a4.js"];
+export const imports = ["_app/immutable/nodes/1.9cfaaf70.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.2226f978.js","_app/immutable/chunks/singletons.9e56b2bd.js"];
 export const stylesheets = [];
 export const fonts = [];
--- src/routes/work/durham-pcc/+page.svelte
+++ src/routes/work/durham-pcc/+page.svelte
@@ -11,12 +11,12 @@
     name="description"
     content="This page showcases the Durham Police and Crime Commissioner website that was build to help the service to offer services to help support people in the community."
   />
-  <meta property="og:image" content="./images/open-graph.jpg" />
+  <meta property="og:image" content="/images/open-graph.jpg" />
   <meta property="og:image:width" content="1200" />
   <meta property="og:image:height" content="630" />
   <meta property="og:image:alt" content="Dove Design Ltd" />
   <meta property="og:image:type" content="image/jpeg" />
-  <meta property="og:image:secure_url" content="./images/open-graph.jpg" />
+  <meta property="og:image:secure_url" content="/images/open-graph.jpg" />
 </svelte:head>
 
 <section>
