3b9ef0ebf7703a7489f35faafad0084a253bfb4c
Dave Dove <info@dovedesign.io>
2025-01-22 13:04:22 +0000

    changed meta path to FAQ image

--- .netlify/server/nodes/4.js
+++ .netlify/server/nodes/4.js
@@ -3,6 +3,6 @@
 export const index = 4;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/faq/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.96f36b33.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.2226f978.js","_app/immutable/chunks/each.e59479a4.js"];
+export const imports = ["_app/immutable/nodes/4.fb100974.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.2226f978.js","_app/immutable/chunks/each.e59479a4.js"];
 export const stylesheets = ["_app/immutable/assets/4.44d0fa1f.css"];
 export const fonts = [];
--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.f4690b71.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.2226f978.js","_app/immutable/chunks/singletons.b643d907.js"];
+export const imports = ["_app/immutable/nodes/1.5c8c513f.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.2226f978.js","_app/immutable/chunks/singletons.c0a3af09.js"];
 export const stylesheets = [];
 export const fonts = [];
