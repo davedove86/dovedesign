2140fbde86efc6a35472adcafb174216fd88069d
Dave Dove <info@dovedesign.io>
2025-01-22 13:28:37 +0000

    root file name changed

--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.cfc66772.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.2226f978.js","_app/immutable/chunks/singletons.c8f185a7.js"];
+export const imports = ["_app/immutable/nodes/1.a04ce164.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.2226f978.js","_app/immutable/chunks/singletons.5ea8ff6d.js"];
 export const stylesheets = [];
 export const fonts = [];
--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -11,12 +11,12 @@
     name="description"
     content="Got questions about web design? Visit Dove Design Ltd's FAQ page for answers to the most common questions about our process, pricing, timeline, and more."
   />
-  <meta property="og:image" content="../../open-graph.jpg" />
+  <meta property="og:image" content="/images/open-graph.jpg" />
   <meta property="og:image:width" content="1200" />
   <meta property="og:image:height" content="630" />
   <meta property="og:image:alt" content="Dove Design Ltd" />
   <meta property="og:image:type" content="image/jpeg" />
-  <meta property="og:image:secure_url" content="../../open-graph.jpg" />
+  <meta property="og:image:secure_url" content="/images/open-graph.jpg" />
 </svelte:head>
 
 <section>
