eebe7d48841f224140d93164cc17b4f8839affde
Dave Dove <info@dovedesign.io>
2025-01-22 13:14:48 +0000

    changed path for FAQ page

--- .netlify/server/chunks/internal.js
+++ .netlify/server/chunks/internal.js
@@ -249,7 +249,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1txhtct"
+  version_hash: "1v3nx6d"
 };
 function get_hooks() {
   return {};
